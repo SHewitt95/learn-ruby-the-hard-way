96e4215c7f46fde6b4cf6ccbfcc14484a3132126
SHewitt95 <s.hewitt7@umiami.edu>
2017-07-19 22:51:14 -0400

    ex03

---
+++ ex03/ex03.rb
@@ -0,0 +1,17 @@
+# Time for some Math!
+puts "the sum of 3 + 5 is #{3 + 5}"
+puts "I'm quite a fan of how Ruby can insert values into strings"
+puts 3 + 6 / 8 * 3 + 50 % 7
+puts "That last number is insane!"
+
+puts "I know that 2 is less than 3, but does Ruby know?"
+puts 2 < 3
+puts 2 > 3
+
+puts "Time for something tricky..."
+puts 3*5>=15
+puts "You may not see it, but there are no spaces..."
+puts "Was that actually tricky? #{2>3}"
+
+puts "Who wants some floats?"
+puts 3.2 * 6.7
