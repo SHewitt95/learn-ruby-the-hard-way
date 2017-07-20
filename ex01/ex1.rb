0d8660af434189b541198dbd8e15ee61d8da3eb6
SHewitt95 <s.hewitt7@umiami.edu>
2017-07-19 22:34:34 -0400

    Finish ex01

---
+++ ex01/ex1.rb
@@ -0,0 +1,7 @@
+puts "Hello Ruby!"
+#puts "This should be interesting"
+#puts "Jimmy's gonna love what I'm doing!"
+#puts "He'd be so proud"
+#puts "It'd help if he existed, though"
+#puts "Instead, he's a figment of my imagination"
+#puts "So it goes, I guess."
