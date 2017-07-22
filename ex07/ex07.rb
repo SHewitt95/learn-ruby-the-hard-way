192f98ea43bc4088bbcec6384a344efad2b9a13b
SHewitt95 <s.hewitt7@umiami.edu>
2017-07-22 15:48:46 -0400

    ex07

---
+++ ex07/ex07.rb
@@ -0,0 +1,15 @@
+puts "Mary had a little lamb."
+puts "whose fleece was white as #{'snow'}"
+puts "And everywhere that Mary went"
+puts "." * 10
+
+h = 'h'
+a = 'a'
+m = 'm'
+
+c = 'c'
+k = 'k'
+e = 'e'
+
+print h + a + m # no new line
+puts c + a + k + e
