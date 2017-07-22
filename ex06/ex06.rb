09f120973087fe78c90e89838a80015a2bdc0e5b
SHewitt95 <s.hewitt7@umiami.edu>
2017-07-22 15:37:10 -0400

    ex06

---
+++ ex06/ex06.rb
@@ -0,0 +1,22 @@
+types_of_people = 10
+puts "There are #{types_of_people} types of people in the world."
+binary = "binary"
+dont = "don't"
+x = "There are those who know #{binary}."
+y = "And there are those who #{dont}."
+
+puts x
+puts y
+
+puts "I said: #{x}"
+puts "I also said: #{y}"
+
+truth = true
+it_hurts = "This joke isn't funny. #{truth}"
+
+puts it_hurts
+
+so_much = ". Why are we still here? Just to suffer?"
+
+# I must've been so clever for this line of code.
+puts it_hurts + so_much
