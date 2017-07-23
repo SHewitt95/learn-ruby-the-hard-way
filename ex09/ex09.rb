64c6e7e1586fe4563b69d1d038965a1312db157f
SHewitt95 <s.hewitt7@umiami.edu>
2017-07-23 13:02:08 -0400

    ex09

---
+++ ex09/ex09.rb
@@ -0,0 +1,13 @@
+days = "Mon Tues Wed Thurs Fri Sat Sun"
+months = "\nJan\nFeb\nMarch\nApril\nMay\nJune\nJuly\nAugust\nSept\nOct\nNov\nDec"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+    This is some long text!
+    It can go many lines.
+    Truly an amazing discovery.
+    What more will I learn about Ruby?
+    It's a quote!
+}
