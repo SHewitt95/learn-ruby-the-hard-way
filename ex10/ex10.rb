e94e4976bfa052aad8d30431be7fbbf4497ce3cc
SHewitt95 <s.hewitt7@umiami.edu>
2017-07-23 13:19:14 -0400

    ex10

---
+++ ex10/ex10.rb
@@ -0,0 +1,16 @@
+tabbed_sentence = "\tI am tabbed!"
+new_line = "I am now\non a new line!"
+backslash_cat = "I am a \\cat\\"
+
+puts tabbed_sentence
+puts new_line
+puts backslash_cat
+
+fat_cat = """
+I'll do a list:
+    \t* Cat food
+    \t* More cat food
+    \t* Even more!\n\t* And taxes
+"""
+
+puts fat_cat
